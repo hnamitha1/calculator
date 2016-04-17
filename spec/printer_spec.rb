685f64ca182d53693050b7289d46ce20534a25c0
hnamitha1 <namitha.h@gmail.com>
2016-04-17 21:55:46 +0100

    added pretty print to printer class

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,5 +1,8 @@
 require 'printer'
 
 describe Printer do
-
+  subject(:printer) {described_class.new}
+  it 'produces a pretty string' do
+    expect(printer.pretty_print(2)).to eq "The answer is 2"
+  end
 end
